bcfd53eee6c4f7e5a85eb3d958247cb0c30f5ec8
Somnath Paul <Somnath4x@gmail.com>
2020-04-22 18:19:22 +0530

    modified

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.style.scss';
+import Directory from '../components/directory/directory.component';
+
+
+const Homepage = () => (
+    <Directory />
+);
+
+export default Homepage;
